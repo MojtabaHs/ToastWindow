4bdec7298b27d0aac9ab67473cc5c181651e4c13
Seyed Mojtaba Hosseini Zeidabadi <chenzook+github@gmail.com>
2025-10-09 01:50:17 +0330

    feat: merge similar apis and update the docs

--- Sources/ToastWindow/ToastManager.swift
+++ Sources/ToastWindow/ToastManager.swift
@@ -20,34 +20,19 @@ public final class ToastManager: Sendable {
     
     public init() { }
     
-    /// Shows a toast notification with the specified content and duration.
-    /// - Parameters:
-    ///   - content: The SwiftUI view to display in the toast.
-    ///   - duration: The time interval after which the toast will automatically dismiss.
-    ///   - isUserInteractionEnabled: Whether the toast should respond to user interactions.
-    ///   - onDismiss: A closure to be called when the toast is dismissed.
-    @MainActor public func showToast<V: View>(content: V,
-                                              duration: TimeInterval,
-                                              isUserInteractionEnabled: Bool = true,
-                                              onDismiss: (() -> Void)? = nil) {
-        
-        WindowManager.createToastWindow(content: content,
-                                        duration: duration,
-                                        id: ToastID(),
-                                        isUserInteractionEnabled: isUserInteractionEnabled,
-                                        onDismiss: onDismiss)
-    }
-    
     /// Shows a toast notification with the specified content and duration, returning its ID.
     /// - Parameters:
     ///   - content: The SwiftUI view to display in the toast.
     ///   - duration: The time interval after which the toast will automatically dismiss.
+    ///               Passing `nil` causes the toast remains visible until manually dismissed.
+    ///               The default value is `2.5`.
     ///   - id: A unique identifier for the toast. If not provided, a new UUID will be generated.
     ///   - isUserInteractionEnabled: Whether the toast should respond to user interactions.
     ///   - onDismiss: A closure to be called when the toast is dismissed.
     /// - Returns: The unique identifier of the created toast which can be used to dismiss it.
+    @discardableResult
     @MainActor public func showToast<V: View>(content: V,
-                                              duration: TimeInterval?,
+                                              duration: TimeInterval? = 2.5,
                                               id: ToastID = ToastID(),
                                               isUserInteractionEnabled: Bool = true,
                                               onDismiss: (() -> Void)? = nil) -> UUID {
@@ -58,24 +43,6 @@ public final class ToastManager: Sendable {
                                         isUserInteractionEnabled: isUserInteractionEnabled,
                                         onDismiss: onDismiss)
         return id
-    }
-    
-    /// Shows a toast notification with the specified content that remains visible until manually dismissed.
-    /// - Parameters:
-    ///   - content: The SwiftUI view to display in the toast.
-    ///   - id: A unique identifier for the toast. If not provided, a new UUID will be generated.
-    ///   - isUserInteractionEnabled: Whether the toast should respond to user interactions.
-    ///   - onDismiss: A closure to be called when the toast is dismissed.
-    @MainActor public func showToast<V: View>(content: V,
-                                              id: ToastID = ToastID(),
-                                              isUserInteractionEnabled: Bool = true,
-                                              onDismiss: DismissClosure? = nil) {
-        
-        WindowManager.createToastWindow(content: content,
-                                        duration: nil,
-                                        id: id,
-                                        isUserInteractionEnabled: isUserInteractionEnabled,
-                                        onDismiss: onDismiss)
     }
     
     /// Shows a toast notification with the specified content and returns a closure to dismiss it.
